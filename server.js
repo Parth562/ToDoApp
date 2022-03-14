4420182529cc5acd9a6d62c5b454b7bf950e3141
Manish giri <manishgiri562@gmail.com>
2022-03-14 11:13:19 +0530

    Improved

--- server.js
+++ server.js
@@ -7,20 +7,17 @@ dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
 );
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('DB connection successful'))
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB connection successful'))
+    .catch((err) => {
+        console.log(err);
+    });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App runing at port ${port}...`);
+    console.log(`App runing at port ${port}...`);
 });
